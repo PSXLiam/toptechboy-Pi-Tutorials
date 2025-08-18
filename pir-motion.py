816f9baf8c292df8f67a0f4b49e5300a00f55722
Liam Rogers <LiamRogersUK@gmail.com>
2025-08-18 15:26:24 +0100

    Modified code for Lesson 24: Implement LED to circuit/code

--- pir-motion.py
+++ pir-motion.py
@@ -2,12 +2,18 @@ import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setmode(GPIO.BCM)
 motionPin = 18
+ledPin = 21
 GPIO.setup (motionPin, GPIO.IN)
+GPIO.setup (ledPin, GPIO.OUT)
 sleep(10)
 try:
     while True:
         motion = GPIO.input(motionPin)
         print(motion)
+        if motion == 1:
+            GPIO.output(ledPin, 1)
+        else:
+            GPIO.output(ledPin, 0)
         sleep(.1)
 except KeyboardInterrupt:
     GPIO.cleanup()
