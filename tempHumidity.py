5fba9906ddc327804714439adfc587e5ed64e8c6
Liam Rogers <LiamRogersUK@gmail.com>
2025-08-19 13:50:14 +0100

    Attempt at code for Lesson 25: Measure Temperature and Humidity with the DHT-11 Sensor Library Issues

---
+++ tempHumidity.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import dht11
+from time import sleep
+GPIO.setmode(GPIO.BCM)
+myDHT = dht11.DHT11(pin = 17)
+try:
+    while True:
+        result = myDHT.read()
+        if result.is_valid():
+            print('Temperature is: ' ,result.tempearature, 'Humiduty is: ',result.humidity)
+        sleep(.2)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print('GPIO Clean!')
+    
