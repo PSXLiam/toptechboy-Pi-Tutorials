70c582fb87184731e2dd6bbc60d4f50baf325abd
Liam Rogers <LiamRogersUK@gmail.com>
2025-09-03 16:26:05 +0100

    Code for Lesson 32: How to Incorporate a Keypad into a Raspberry Pi Project Fix toggle to buttons

--- keypad2.py
+++ keypad2.py
@@ -7,7 +7,8 @@ GPIO.setmode(GPIO.BOARD)
 rows = [11,13,15,29]
 columns = [31,33,35,37]
 keypad = [[1,2,3,'A'],[4,5,6,'B'],[7,8,9,'C'],['+',0,'#','D']]
-butValOld = 0
+press = 0
+pressOld = 0
 
 #Setup
 GPIO.setup(rows[0], GPIO.OUT)
@@ -21,14 +22,17 @@ GPIO.setup(columns[3], GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 try:
     while True:
+        press = 0
         for myRow in [0,1,2,3]:
             for myColumn in [0,1,2,3]:
                 GPIO.output(rows[myRow], GPIO.HIGH)
                 butVal = GPIO.input(columns[myColumn])
                 GPIO.output(rows[myRow], GPIO.LOW)
-                if butVal == 1 and butValOld == 0:
-                    print(keypad[myRow][myColumn])
-                butValOld = butVal
+                if butVal == 1:
+                    press = 1
+                if butVal == 1 and press == 1 and pressOld == 0:
+                    print(keypad[myRow][myColumn])      
+        pressOld = press
         sleep(.2)
 except KeyboardInterrupt:
     sleep(.1)
