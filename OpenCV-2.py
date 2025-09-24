edf8cc790367743a7f8fd9f69e921d2040c1b372
Liam Rogers <LiamRogersUK@gmail.com>
2025-09-24 16:21:19 +0100

    Completed code for Lesson 46: Calculating Frames Per Second (FPS) In OpenCV and Overlay on Frame Organise code and implement variables for values

--- OpenCV-2.py
+++ OpenCV-2.py
@@ -3,19 +3,29 @@ import cv2
 from picamera2 import Picamera2
 import time
 
+#Variables
+dispW = 640
+dispH = 360
+fps = 0
+pos = (30, 60)
+font = cv2.FONT_HERSHEY_SIMPLEX
+height = 1.0
+myColor = (0, 0, 255)
+weight = 3
+
+#Camera Setup
 piCam = Picamera2()
-piCam.preview_configuration.main.size = (640, 360)
+piCam.preview_configuration.main.size = (dispW, dispH)
 piCam.preview_configuration.main.format = "RGB888"
 piCam.preview_configuration.controls.FrameRate = 30
 piCam.preview_configuration.align()
 piCam.configure("preview")
 piCam.start()
-fps = 0
 
 while True:
     tStart = time.time()
     frame = piCam.capture_array()
-    cv2.putText(frame, str(int(fps)), (30, 60), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,0,255), 3)
+    cv2.putText(frame, str(int(fps)) + ' FPS', pos, font, height, myColor, weight)
     cv2.imshow("piCam", frame)
     if cv2.waitKey(1) == ord('q'):
         break
