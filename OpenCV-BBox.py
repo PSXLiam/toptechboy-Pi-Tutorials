5ba7b262850dbf917f33325fbaa11fa050789a8b
Liam Rogers <LiamRogersUK@gmail.com>
2025-09-29 15:07:13 +0100

    Code for Lesson 48: Creating A Bouncing Box Overlay in OpenCV Copy OpenCV-Shapes.py from Lesson 47

---
+++ OpenCV-BBox.py
@@ -0,0 +1,50 @@
+#Imports
+import cv2
+from picamera2 import Picamera2
+import time
+
+#Variables
+dispW = 640
+dispH = 360
+fps = 0
+pos = (30, 60)
+font = cv2.FONT_HERSHEY_SIMPLEX
+height = 1.0
+myColor = (0, 0, 255)
+weight = 3
+
+#Rectangle Variables
+upperLeft = (540, 150)
+lowerRight = (640, 225)
+rColor = (255, 255, 255)
+thickness = 5
+
+#Circle Variables
+center = (320, 180)
+radius = 35
+cColor = (0, 255, 255)
+cThickness = 5
+
+#Camera Setup
+piCam = Picamera2()
+piCam.preview_configuration.main.size = (dispW, dispH)
+piCam.preview_configuration.main.format = "RGB888"
+piCam.preview_configuration.controls.FrameRate = 30
+piCam.preview_configuration.align()
+piCam.configure("preview")
+piCam.start()
+
+while True:
+    tStart = time.time()
+    frame = piCam.capture_array()
+    cv2.putText(frame, str(int(fps)) + ' FPS', pos, font, height, myColor, weight)
+    cv2.rectangle(frame, upperLeft, lowerRight, rColor, thickness)
+    cv2.circle(frame, center, radius, cColor, cThickness)
+    cv2.imshow("piCam", frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+    tEnd = time.time()
+    loopTime = tEnd - tStart
+    fps = .9*fps + .1*(1/loopTime)
+    #print(int(fps))
+cv2.destroyAllWindows()
