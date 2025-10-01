995970a5d018b82c9ab029d5ad603c725dac9e28
Liam Rogers <LiamRogersUK@gmail.com>
2025-10-01 16:39:14 +0100

    Code for Lesson 50: Modifying OpenCV Images and Creating Regions of Interest Create green region of the entire top right quadrant of feed

--- OpenCV-RoI.py
+++ OpenCV-RoI.py
@@ -25,7 +25,7 @@ piCam.start()
 while True:
     tStart = time.time()
     frame = piCam.capture_array()
-    frame[int(dispH/2):,int(dispW/2):] = [0,255,0]
+    frame[:int(dispH/2),int(dispW/2):] = [0,255,0]
     cv2.putText(frame, str(int(fps)) + ' FPS', pos, font, height, myColor, weight)
     cv2.imshow("piCam", frame)
     if cv2.waitKey(1) == ord('q'):
