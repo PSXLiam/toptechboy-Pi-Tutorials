a61b077c3e5470608e925e56c034c5f5cec4f7be
Liam Rogers <LiamRogersUK@gmail.com>
2025-10-15 16:02:50 +0100

    Code for Lesson 56: Assembling the Sunfounder Pan/Tilt Hat for the RPi Camera Create basic program to convert angles to servo inputs

---
+++ camServo.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setmode(GPIO.BCM)
+pwmPin = 18
+GPIO.setup(pwmPin, GPIO.OUT)
+pwm = GPIO.PWM(pwmPin, 50)
+pwm.start(0)
+try:
+    while True:
+        pwmAngle = float(input('Angle % '))
+        pwmPercent = (-1/18)*pwmAngle+7
+        if pwmPercent > 12:
+            pwmPercent = 12
+        if pwmPercent < 2:
+            pwmPercent = 2
+        pwm.ChangeDutyCycle(pwmPercent)
+        sleep(.1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print('GPIO Clean!')
