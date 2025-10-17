87d6565def0b21e35f545aec6d4bc5f645ace06a
Liam Rogers <LiamRogersUK@gmail.com>
2025-10-17 15:53:13 +0100

    Code for Lesson 56: Assembling the Sunfounder Pan/Tilt Hat for the RPi Camera Make camServo into a function

--- camServo.py
+++ camServo.py
@@ -5,16 +5,22 @@ pwmPin = 18
 GPIO.setup(pwmPin, GPIO.OUT)
 pwm = GPIO.PWM(pwmPin, 50)
 pwm.start(0)
+
+def camServo():
+    pwmAngle = float(input('Angle % '))
+    pwmPercent = (-1/18)*pwmAngle+7
+    if pwmPercent > 12:
+        pwmPercent = 12
+    if pwmPercent < 2:
+        pwmPercent = 2
+    pwm.ChangeDutyCycle(pwmPercent)
+    sleep(.1)
+
+
 try:
     while True:
-        pwmAngle = float(input('Angle % '))
-        pwmPercent = (-1/18)*pwmAngle+7
-        if pwmPercent > 12:
-            pwmPercent = 12
-        if pwmPercent < 2:
-            pwmPercent = 2
-        pwm.ChangeDutyCycle(pwmPercent)
-        sleep(.1)
+        camServo()
+        
 except KeyboardInterrupt:
     GPIO.cleanup()
     print('GPIO Clean!')
