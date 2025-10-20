4b39cb450a97a7aab1a11d57483efadccbb99953
Liam Rogers <LiamRogersUK@gmail.com>
2025-10-20 16:57:17 +0100

    Completed code for Lesson 56: Assembling the Sunfounder Pan/Tilt Hat for the RPi Camera Create Servo class, add functions for get_angle and GPIO_Clean then implement class to main code

--- camServo-zero.py
+++ camServo-zero.py
@@ -1,17 +1,13 @@
-import RPi.GPIO as GPIO
-from camServo import set_angle
+from camServo import Servo
 from time import sleep
-GPIO.setmode(GPIO.BCM)
-pwmPin = 18
-GPIO.setup(pwmPin, GPIO.OUT)
-pwm = GPIO.PWM(pwmPin, 50)
-pwm.start(0)
+
+pan = Servo()
 
 try:
     while True:
         angle = float(input('Angle % '))
-        set_angle(pwm, angle)
-        
+        pan.set_angle(angle)
+        print (pan.get_angle())
+
 except KeyboardInterrupt:
-    GPIO.cleanup()
-    print('GPIO Clean!')
+    pan.GPIO_Clean()
--- camServo.py
+++ camServo.py
@@ -1,10 +1,31 @@
+import RPi.GPIO as GPIO
 from time import sleep
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
 
-def set_angle(pwm, pwmAngle):
-    pwmPercent = (-1/18)*pwmAngle+7
-    if pwmPercent > 12:
-        pwmPercent = 12
-    if pwmPercent < 2:
-        pwmPercent = 2
-    pwm.ChangeDutyCycle(pwmPercent)
-    sleep(.1)
+class Servo():
+    
+    def __init__(self, min_angle=-90, max_angle=90):
+
+        self.pwm = GPIO.PWM(18, 50)
+        self.pwm.start(0)
+        self.angle = 0
+        self.min_angle = min_angle
+        self.max_angle = max_angle
+
+    def set_angle(self, pwmAngle):
+        self.angle =  pwmAngle
+        pwmPercent = (-1/18)*pwmAngle+7
+        if pwmPercent > 12:
+            pwmPercent = 12
+        if pwmPercent < 2:
+            pwmPercent = 2
+        self.pwm.ChangeDutyCycle(pwmPercent)
+        sleep(.1)
+        
+    def get_angle(self):
+        return self.angle
+    
+    def GPIO_Clean(self):
+        GPIO.cleanup()
+        print('GPIO Clean!')
